2d0603f0e222b7d78a0b6c251b002e7aff9876f7
elias <elias@gmail.com>
2026-05-10 23:25:04 -0700

    JoinMeetingScreen Update

--- components/JoinMeetingScreen.tsx
+++ components/JoinMeetingScreen.tsx
@@ -380,6 +380,9 @@ function YouTubeIconLink({ href }: { href: string }) {
       >
         <path d="M23.5 6.2a3 3 0 0 0-2.1-2.1C19.6 3.6 12 3.6 12 3.6s-7.6 0-9.4.5A3 3 0 0 0 .5 6.2 31 31 0 0 0 0 12a31 31 0 0 0 .5 5.8 3 3 0 0 0 2.1 2.1c1.8.5 9.4.5 9.4.5s7.6 0 9.4-.5a3 3 0 0 0 2.1-2.1A31 31 0 0 0 24 12a31 31 0 0 0-.5-5.8zM9.6 15.5v-7l6.2 3.5-6.2 3.5z" />
       </svg>
+      <span className="text-xs font-medium">
+        Show how
+      </span>
     </a>
   );
 }
